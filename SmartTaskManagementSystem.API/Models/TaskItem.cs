cc7f74d916a321ff9c63f2ad68bda3f974c5de10
vguru shetty <vgurushetty@gmail.com>
2026-04-29 22:18:23 +0530

    update

--- SmartTaskManagementSystem.API/Models/TaskItem.cs
+++ SmartTaskManagementSystem.API/Models/TaskItem.cs
@@ -2,7 +2,7 @@
 {
     public class TaskItem
     {
-        //class propertiesv
+        //class properties
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
